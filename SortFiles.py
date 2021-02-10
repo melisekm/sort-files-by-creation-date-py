6b7a5b5448466bd8b266b0eca5ab59c402382fe2
Martin Melišek <46221266+melisekm@users.noreply.github.com>
2021-02-10 02:07:23 +0100

    slight refactor

--- SortFiles.py
+++ SortFiles.py
@@ -1,15 +1,32 @@
 import os
 from datetime import datetime
 
-with os.scandir() as dir_entries:
-    os.mkdir(".\\Sorted")
-    for entry in dir_entries:
-        if str(entry.name).endswith(".jpg") or str(entry.name).endswith(".JPG") or str(entry.name).endswith(".mp4") or str(entry.name).endswith(".MP4"):
-            casVytvorenia = entry.stat().st_mtime
-            try:
-                nazovPriecinku = ".\\Sorted\\" + datetime.utcfromtimestamp(casVytvorenia).strftime('%Y-%m-%d')
-                os.mkdir(nazovPriecinku)
-            except: pass
-            finally:
-                nazovSuboru = nazovPriecinku + "\\" + entry.name
-                os.replace(entry.path, nazovSuboru)
+extensions = [".jpg", ".mp4", ".png"]
+
+
+def get_extension(name):
+    _, file_extension = os.path.splitext(name)
+    return file_extension
+
+
+def run():
+    with os.scandir() as dir_entries:
+        os.mkdir(".\\Sorted")
+        for entry in dir_entries:
+            extension = get_extension(entry.name)
+            if extension.casefold() in extensions:
+                creation_time = entry.stat().st_mtime
+                try:
+                    directory_name = ".\\Sorted\\" + datetime.utcfromtimestamp(creation_time).strftime(
+                        "%Y-%m-%d"
+                    )
+                    os.mkdir(directory_name)
+                except OSError:
+                    pass
+                finally:
+                    file_name = directory_name + "\\" + entry.name
+                    os.replace(entry.path, file_name)
+
+
+if __name__ == "__main__":
+    run()
